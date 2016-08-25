ed1567895ddd391f537ee84e9a7b77bbd5592358
渡邊元晴 <motoharu.watanabe@learn-earn.co.jp>
2016-08-26 02:12:58 +0900

    課題再提出 ・入力時の小数点入力を可能に ・未入力時のアラート表示

--- app/src/main/java/jp/techacademy/motoharu/watanabe/calcapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/motoharu/watanabe/calcapp/MainActivity.java
@@ -2,13 +2,14 @@ package jp.techacademy.motoharu.watanabe.calcapp;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
+import android.widget.TextView.BufferType;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -34,31 +35,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button4.setOnClickListener(this);
 
         mEditText1 = (EditText) findViewById(R.id.editText1);
-        mEditText1.setInputType(InputType.TYPE_CLASS_NUMBER);   //数値入力指定
+        mEditText1.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);   //小数点含めた数値入力指定
+
         mEditText2 = (EditText) findViewById(R.id.editText2);
-        mEditText2.setInputType(InputType.TYPE_CLASS_NUMBER);   //数値入力指定
+        mEditText2.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
     }
 
     @Override
     public void onClick(View v){
 
-        //String→doubleに変換
         String result_text1 = mEditText1.getText().toString();
+        if(result_text1.length()==0){
+            new AlertDialog.Builder(this)
+                    .setTitle("アラート")
+                    .setMessage("数値が未入力です。")
+                    .setPositiveButton("OK", null)
+                    .show();
+            return;
+        }
         String result_text2 = mEditText2.getText().toString();
+        if(result_text1.length()==0) {
+            new AlertDialog.Builder(this)
+                    .setTitle("アラート")
+                    .setMessage("数値が未入力です。")
+                    .setPositiveButton("OK", null)
+                    .show();
+            return;
+        }
+        //String→doubleに変換
         double result_double1 = Double.parseDouble(result_text1);
         double result_double2 = Double.parseDouble(result_text2);
 
         Intent intent = new Intent(this,SecondActivity.class);
 
         //引数分岐
-        if(v.getId()==R.id.button1){
-            intent.putExtra("VALUE1",result_double1+result_double2);
-        }else if(v.getId()==R.id.button2){
-            intent.putExtra("VALUE1",result_double1-result_double2);
-        }else if(v.getId()==R.id.button3){
-            intent.putExtra("VALUE1",result_double1*result_double2);
-        }else if(v.getId()==R.id.button4) {
-            intent.putExtra("VALUE1",result_double1/result_double2);
+        if (v.getId() == R.id.button1) {
+            intent.putExtra("VALUE1", result_double1 + result_double2);
+        } else if (v.getId() == R.id.button2) {
+            intent.putExtra("VALUE1", result_double1 - result_double2);
+        } else if (v.getId() == R.id.button3) {
+            intent.putExtra("VALUE1", result_double1 * result_double2);
+        } else if (v.getId() == R.id.button4) {
+            intent.putExtra("VALUE1", result_double1 / result_double2);
         }
 
         //画面遷移
